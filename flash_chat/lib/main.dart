b56dffc63110de744683ae6d1deab18aad7a9ac7
zienex <szymon.zienkiewicz5@gmail.com>
2024-08-14 15:50:42 +0200

    Initial commit

---
+++ flash_chat/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'chat_screen': (context) => ChatScreen()
+      },
+    );
+  }
+}
